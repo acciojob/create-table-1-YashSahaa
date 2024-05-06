c813cb5ddd654c98c19ca900757acd06feff2318
YashSahaa <136882182+YashSahaa@users.noreply.github.com>
2024-05-06 11:09:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 function insert_Row() {
     //Write your code here
-	let addrow = document.getElementById("sampleTable")
-	let row = document.createElement("tr")
-	row.innerHTML = `
-		<td>New Cell1</td> 
-		<td>New Cell2</td>
-	`
-	addrow.prepend(row)
+	let table = document.getElementById("sampleTable");
+    let row = table.insertRow(0);
+    let cell1 = row.insertCell(0);
+    let cell2 = row.insertCell(1);
+    cell1.textContent = "New Cell1";
+    cell2.textContent = "New Cell2";
+	// let addrow = document.getElementById("sampleTable")
+	// let row = document.createElement("tr")
+	// row.innerHTML = `
+	// 	<td>New Cell1</td> 
+	// 	<td>New Cell2</td>
+	// `
+	// addrow.prepend(row)
 }
